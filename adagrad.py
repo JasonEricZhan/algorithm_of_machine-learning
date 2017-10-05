97402a3b500ae2a6365ab291de2fc3003279da1e
Chan Yao Chun(Eric) <jan11781202@gmail.com>
2017-10-05 20:52:16 +0800

    Update adagrad.py

--- adagrad.py
+++ adagrad.py
@@ -1,4 +1,4 @@
-def ada_grad(eta,ada_value,gradient):
+def compute(eta,ada_value,gradient):
     ada_value=ada_value+gradient**2
     ada_direction=(eta/np.sqrt(ada_value+1e-8))*gradient
     return  ada_direction,ada_value
