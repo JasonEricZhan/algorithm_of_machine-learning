1484d69754c0ccd453088ecc4296f8ec3ce066fe
Chan Yao Chun(Eric) <jan11781202@gmail.com>
2017-10-06 10:08:52 +0800

    Update adagrad.py

--- adagrad.py
+++ adagrad.py
@@ -1,7 +1,7 @@
-def compute(eta,ada_value,gradient):
-    ada_value=ada_value+gradient**2
-    ada_direction=(eta/np.sqrt(ada_value+1e-8))*gradient
-    return  ada_direction,ada_value
+def compute(eta,ada_sum,gradient):
+    ada_sum=ada_sum+gradient**2
+    ada_direction=(eta/np.sqrt(ada_sum+1e-8))*gradient
+    return  ada_direction,ada_sum
 
 """
 initailize w is important
